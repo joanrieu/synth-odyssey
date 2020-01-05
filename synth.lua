a9f6c0b4eb9038f4a139558bbf6e512c72738550
Joan Rieu <joan@joanrieu.com>
2020-01-05 13:18:18 -0500

    AR envelope

--- synth.lua
+++ synth.lua
@@ -6,11 +6,13 @@ sr = 44100
 cv_to_hz = 440.0 / 8.0
 
 function run()
+    ar_attack = 0.99
+    ar_release = 0.9999
     vco2_tune = 3.0
     vco2_sync = true
     vcf_cutoff = 40.0
     vcf_reso = 0.2
-    vca_gain = 4.0
+    vca_env = 4.0
 
     while true do
         update()
@@ -22,6 +24,7 @@ end
 
 function update()
     update_kbd()
+    update_ar()
     update_vco1()
     update_vco2()
     update_mix()
@@ -35,7 +38,7 @@ end
 --------------------------------------------------------------------------------
 
 -- out
-kbd_trigger = true
+kbd_trigger = false
 kbd_gate = false
 kbd_cv = 0.0
 
@@ -54,7 +57,7 @@ update_kbd = coroutine.wrap(
             for i = 1, #melody do
                 for j = 1, samples_per_note do
                     kbd_trigger = j == 1
-                    kbd_gate = j < samples_per_note * 7 / 8
+                    kbd_gate = j < samples_per_note / 5
                     kbd_cv = melody[i]
                     coroutine.yield()
                 end
@@ -62,6 +65,26 @@ update_kbd = coroutine.wrap(
         end
     end
 )
+
+--------------------------------------------------------------------------------
+-- AR
+--------------------------------------------------------------------------------
+
+-- knobs
+ar_attack = 0.0
+ar_release = 0.0
+
+-- out
+ar_out = 0.0
+
+-- TODO: use sample rate in computation
+function update_ar()
+    if kbd_gate then
+        ar_out = ar_attack * ar_out + (1 - ar_attack)
+    else
+        ar_out = ar_release * ar_out
+    end
+end
 
 --------------------------------------------------------------------------------
 -- VCO 1
@@ -78,22 +101,13 @@ vco1_sync = false
 
 function update_vco1()
     vco1_sync = false
-    if kbd_trigger then
-        vco1_sync = true
+    local step = 2 * kbd_cv * cv_to_hz * (vco1_tune + vco1_detune / 100) / sr
+    vco1_saw = vco1_saw + step
+    if vco1_saw >= 1 then
         vco1_saw = -1
-        vco1_square = -1
-    elseif kbd_gate then
-        local step = 2 * kbd_cv * cv_to_hz * (vco1_tune + vco1_detune / 100) / sr
-        vco1_saw = vco1_saw + step
-        if vco1_saw >= 1 then
-            vco1_saw = -1
-            vco1_sync = true
-        end
-        vco1_square = vco1_saw > 0 and 1 or -1
-    else
-        vco1_saw = 0
-        vco1_square = 0
+        vco1_sync = true
     end
+    vco1_square = vco1_saw > 0 and 1 or -1
 end
 
 --------------------------------------------------------------------------------
@@ -110,21 +124,16 @@ vco2_saw = 0.0
 vco2_square = 0.0
 
 function update_vco2()
-    if kbd_gate then
-        if vco2_sync and vco1_sync then
-            vco2_saw = -1
-        else
-            local step = 2 * kbd_cv * cv_to_hz * (vco2_tune + vco2_detune / 100) / sr
-            vco2_saw = vco2_saw + step
-            if vco2_saw >= 1 then
-                vco2_saw = -1
-            end
-        end
-        vco2_square = vco2_saw > 0 and 1 or -1
+    if vco2_sync and vco1_sync then
+        vco2_saw = -1
     else
-        vco2_saw = 0
-        vco2_square = 0
+        local step = 2 * kbd_cv * cv_to_hz * (vco2_tune + vco2_detune / 100) / sr
+        vco2_saw = vco2_saw + step
+        if vco2_saw >= 1 then
+            vco2_saw = -1
+        end
     end
+    vco2_square = vco2_saw > 0 and 1 or -1
 end
 
 --------------------------------------------------------------------------------
@@ -189,7 +198,7 @@ hpf_low = 0.0
 -- out
 hpf_out = 0.0
 
--- TODO: replace by a better filter
+-- TODO: replace by a better filter (and use sample rate in computation)
 function update_hpf()
     local b = hpf_cutoff
     local a = 1.0 - b
@@ -202,13 +211,15 @@ end
 --------------------------------------------------------------------------------
 
 -- knobs
+vca_env = 0.0
 vca_gain = 0.0
 
 -- out
 vca_out = 0.0
 
 function update_vca()
-    vca_out = math.min(1.0, math.max(-1.0, hpf_out * vca_gain))
+    vca_out = (vca_env * ar_out + vca_gain) * hpf_out
+    vca_out = math.min(1.0, math.max(-1.0, vca_out))
 end
 
 --------------------------------------------------------------------------------
