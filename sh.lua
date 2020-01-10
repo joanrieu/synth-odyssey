4ddd8ca274bf50ba605c10d6eb982efb636e0043
Joan Rieu <joan@joanrieu.com>
2020-01-09 22:37:18 -0500

    sample/hold lag

--- sh.lua
+++ sh.lua
@@ -8,6 +8,10 @@ sh_vco1_saw = true
 sh_noise = 0
 sh_noise_gen = true
 sh_lfo_trigger = true
+sh_lag = 0
+
+-- internal
+sh_target = 0
 
 -- out
 sh_mix = 0
@@ -19,6 +23,8 @@ function update_sh()
     sh_mix = (vco1 + noise_vco2) / math.max(1, sh_vco1 + sh_noise)
     local trigger = sh_lfo_trigger and lfo_trigger or kbd_trigger
     if trigger then
-        sh_out = sh_mix
+        sh_target = sh_mix
     end
+    local a = 1 - 10 ^ (-2 - sh_lag * sr / 2e4)
+    sh_out = a * sh_out + (1 - a) * sh_target
 end
