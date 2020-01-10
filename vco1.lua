a79ef46c0aa0e5ef819797c5239ef2c05af63680
Joan Rieu <joan@joanrieu.com>
2020-01-09 21:50:13 -0500

    VCO1 kbd tracking switch

--- vco1.lua
+++ vco1.lua
@@ -5,6 +5,7 @@
 -- knobs
 vco1_tune = 0
 vco1_detune = 0
+vco1_kbd = true
 vco1_fm1 = 0
 vco1_fm1_is_lfo_sine = true
 vco1_fm2 = 0
@@ -23,7 +24,8 @@ function update_vco1()
     local fm =
         vco1_fm1 * (vco1_fm1_is_lfo_sine and lfo_sine or lfo_square) +
         vco1_fm2 * (vco1_fm2_is_sh_out and sh_out or adsr_out)
-    local step = 2 * kbd_freq * (vco1_tune + vco1_detune / 100 + fm) / sr
+    local freq = vco1_kbd and kbd_freq or 1
+    local step = 2 * freq * (vco1_tune + vco1_detune / 100 + fm) / sr
     vco1_saw = vco1_saw + step
     if vco1_saw >= 1 then
         vco1_saw = -1
