5507ee56515c9d93ee5ba2c485b6ddbf4bc99386
Joan Rieu <joan@joanrieu.com>
2020-01-22 02:23:38 -0500

    fix pink noise generator

--- noise.cpp
+++ noise.cpp
@@ -8,43 +8,29 @@
 
 // Trammel Stochastic Algorithm
 // https://web.archive.org/web/20160705171002/http://home.earthlink.net/~ltrammell/tech/pinkalg.htm
-static float amplitudes[] = {
-    0.24390449452997848,
-    0.3158285620980443,
-    0.4402669433719773
-};
-static float probabilities[] = {
-    3.1878e-1,
-    7.7686e-1,
-    9.7785e-1
-};
-static float pink_band(int* state, float amplitude, float probability, float sample, float random) {
-    crBegin(*state);
-    while (true) {
-        sample = amplitude * (random * 2 -1);
-        do {
-            crReturn(*state, sample);
-        } while (random < probability);
-    }
-    crFinish();
 
-    abort();
+static const float amplitudes[] = { 0.00224, 0.00821, 0.00798, 0.00694, 0.00992 };
+static const float probabilities[] = { 0.15571, 0.30194, 0.74115, 0.93003, 0.98035 };
+
+static inline float random_float_positive() {
+    return rand() / float(RAND_MAX);
+}
+
+static inline float random_float_signed() {
+    return random_float_positive() * 2 - 1;
+}
+
+static inline float pink_band(Noise& noise, int i) {
+    if (random_float_positive() < probabilities[i]) {
+        noise.samples[i] = amplitudes[i] * random_float_signed() / 0.03529;
+    }
+    return noise.samples[i];
 }
 
 void Synth::update_noise() {
-    auto random = rand() / float(RAND_MAX);
     if (noise.white) {
-        noise.out = random * 2 - 1;
+        noise.out = random_float_signed();
     } else {
-        noise.out = 0;
-        for (int i = 0; i < 3; ++i) {
-            noise.out += noise.samples[i] = pink_band(
-                &noise.state[i],
-                amplitudes[i],
-                probabilities[i],
-                noise.samples[i],
-                random
-            );
-        }
+        noise.out = pink_band(noise, 0) + pink_band(noise, 1) + pink_band(noise, 2);
     }
 }
--- noise.hpp
+++ noise.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
-#include "coroutines.hpp"
-
 struct Noise {
     // knobs
     bool
     white = true;
 
     // internal
-    int
-    state[3] = {0};
     float
     samples[3] = {0};
 
