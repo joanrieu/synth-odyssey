b07ed3ab8ccab1ea02752c215341a61b2e412087
Joan Rieu <joan@joanrieu.com>
2020-04-17 19:55:23 -0400

    prevent mixer output overdrive

--- core/mix.cpp
+++ core/mix.cpp
@@ -8,6 +8,7 @@ void Synth::update_mix() {
   auto vco1_wave = mix.vco1_saw ? vco1.saw : vco1.square;
   auto vco2_wave = mix.vco2_saw ? vco2.saw : vco2.square;
   auto noise_ring = mix.noise ? noise.out : vco1_wave * vco2_wave;
-  mix.out =
-      mix.vco1 * vco1_wave + mix.vco2 * vco2_wave + mix.noise_ring * noise_ring;
+  mix.out = (mix.vco1 * vco1_wave + mix.vco2 * vco2_wave +
+             mix.noise_ring * noise_ring) /
+            std::fmax(1.f, mix.vco1 + mix.vco2 + mix.noise_ring);
 }
